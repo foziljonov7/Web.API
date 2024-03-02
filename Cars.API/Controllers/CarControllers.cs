4ff81af51b154b8fff82eff361e03adcb06debb4
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2024-03-02 13:21:41 +0500

    ...

---
+++ Cars.API/Controllers/CarControllers.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cars.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarControllers : ControllerBase
+    {
+        [HttpGet("/")]
+        public IActionResult Hello()
+        {
+            return Ok("Welcome to Car API");
+        }
+    }
+}
