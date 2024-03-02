a1221a4fffbeb70f33e6ac1aabbdbc406c2912fb
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2024-03-02 13:16:29 +0500

    Initial commit

---
+++ Cars.API/Entities/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace Cars.API.Entities
+{
+    public class Car
+    {
+        public Guid Id { get; set; }
+        public string Model { get; set; }
+        public double Price { get; set; }
+        public string Probeg { get; set; }
+        public string Color { get; set; }
+        public string Engine { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ Cars.API/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Cars.API.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
