4ad0d335dc67f8ee289ed6c2ed517d1081bd7e69
salijanova <salijanovamadina635@gmail.com>
2024-03-12 11:51:32 +0300

    Successfully completed #5

---
+++ Cars.API/ViewModels/CarViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Cars.API.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cars.API.ViewModels
+{
+    public class CarViewModel
+    {
+        public Guid Id { get; set; }
+        public string Model { get; set; }
+        public double Price { get; set; }
+        public double Probeg { get; set; }
+        public string Color { get; set; }
+        public string Engine { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public CategoryViewModel Category { get; set; }
+        public Status Status { get; set; }
+
+
+
+        public static explicit operator CarViewModel(Car car)
+        {
+            if (car is null)
+                return null;
+
+            return new CarViewModel
+            {
+                Id = car.Id,
+                Model = car.Model,
+                Price = car.Price,
+                Probeg = car.Probeg,
+                Color = car.Color,
+                Engine = car.Engine,
+                Created = car.Created,
+                Updated = car.Updated,
+                Category = (CategoryViewModel)car.Category,
+                Status = car.Status
+            };
+        }
+    }
+}
---
+++ Cars.API/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Cars.API.Entities;
+
+namespace Cars.API.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static explicit operator CategoryViewModel(Category category)
+        {
+            return new CategoryViewModel
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+        }
+    }
+}
