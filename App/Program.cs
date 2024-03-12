92b96b155492745fc6adbf111f77dab11b8e3e98
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2024-03-12 13:36:37 +0500

    ...

---
+++ App/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Loaded...");
+Console.WriteLine("-------------------------------------");
+using (var httpClient = new HttpClient())
+{
+    var response = await httpClient.GetAsync("https://localhost:7178/");
+
+    if (response.IsSuccessStatusCode)
+    {
+        var content = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(content);
+
+        Console.WriteLine("Successfully\n ----------------------------------------------------");
+    }
+    else
+    {
+        Console.WriteLine("Failed\n -------------------------------------------------");
+    }
+}
+
+
