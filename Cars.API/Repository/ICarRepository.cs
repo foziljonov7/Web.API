6926f55dd3306f1807adfdddb1666d6b6e5b3a88
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2024-03-12 12:51:38 +0500

    Successfully completed task #6

---
+++ Cars.API/Repository/ICarRepository.cs
@@ -0,0 +1,15 @@
+﻿using Cars.API.Dtos;
+using Cars.API.Entities;
+using Cars.API.Response;
+
+namespace Cars.API.Repository
+{
+    public interface ICarRepository
+    {
+        Task<List<Car>> GetCarsAsync();
+        Task<Car> GetCarAsync(Guid id);
+        Task<GeneralResponse> CreateCarAsync(CreateCarDto newCar);
+        Task<GeneralResponse> UpdateCarAsync(Guid id, UpdateCarDto newCar);
+        Task<GeneralResponse> DeleteCarAsync(Guid id);
+    }
+}
