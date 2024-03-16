9d88f7ac884508dc7a9aaf292fcc51971891f9bb
xomidov571 <nodirbekh56@gmail.com>
2024-03-16 14:18:20 +0500

    CategoryRepository #16

---
+++ Cars.API/Repository/CategoryRepository.cs
@@ -0,0 +1,54 @@
+﻿using Cars.API.Data;
+using Cars.API.Entities;
+using Cars.API.Response;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cars.API.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext dbcontext;
+
+        public CategoryRepository(AppDbContext dbcontext)
+        {
+             dbcontext = dbcontext;
+        }
+
+        public async Task<GeneralResponse> CreateCategoryAsync(string name)
+        {
+            if (name is null) 
+                return new GeneralResponse(false, "name is null");
+
+            var category = new Category
+            {
+                Name = name
+            };
+
+            await  dbcontext.Categories.AddAsync(category);
+            await  dbcontext.SaveChangesAsync();
+            return new GeneralResponse(true, "Successfully created");
+        }
+
+        public async Task<List<Category>> GetCategoriesAsync()
+            => await  dbcontext.Categories.ToListAsync();
+
+        public async Task<Category> GetCategoryAsync(int id)
+        {
+            var category = await dbcontext.Categories
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (category is null)
+                return null;
+
+            return category;
+        }
+
+        public async Task<List<Car>> GetCategoryByCarAsync(int categoryId)
+        {
+            var category = await dbcontext.Categories
+                .FirstOrDefaultAsync(c => c.Id == categoryId);
+
+            return null;
+        }
+    }
+}
---
+++ Cars.API/Repository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Cars.API.Entities;
+using Cars.API.Response;
+
+namespace Cars.API.Repository
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetCategoriesAsync();
+        Task<Category> GetCategoryAsync(int id);
+        Task<List<Car>> GetCategoryByCarAsync(int categoryId);
+        Task<GeneralResponse> CreateCategoryAsync(string name);
+    }
+}
