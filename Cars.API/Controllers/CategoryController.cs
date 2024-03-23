28a39a9367208d8e717aefc59e381e4953ffaf54
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2024-03-23 13:48:13 +0500

    Successfully completed task #21

---
+++ Cars.API/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using Cars.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cars.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService service;
+
+        public CategoryController(ICategoryService service)
+            => this.service = service;
+
+        [HttpGet("GetCategories")]
+        public async Task<IActionResult> GetCategoriesAsync()
+            => Ok(await service.GetCategoriesAsync());
+
+        [HttpGet("GetCategory/{id}")]
+        public async Task<IActionResult> GetCategoryAsync([FromRoute] int id)
+            => Ok(await service.GetCategoryAsync(id));
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateCategoryAsync([FromBody] string name)
+            => Ok(await service.CreateCategoryAsync(name));
+
+        [HttpGet("GetByCar/{categoryId}")]
+        public async Task<IActionResult> GetCategoryByCarAsync([FromRoute] int categoryId)
+            => Ok(await service.GetCategoryByCarAsync(categoryId));
+    }
+}
---
+++ Cars.API/Controllers/SoldController.cs
@@ -0,0 +1,30 @@
+﻿using Cars.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cars.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SoldController : ControllerBase
+    {
+        private readonly ISoldService service;
+
+        public SoldController(ISoldService service)
+            => this.service = service;
+
+        [HttpGet("GetSolds")]
+        public async Task<IActionResult> GetSoldsAsync()
+            => Ok(await service.GetSoldsAsync());
+
+        [HttpGet("GetSold/{id}")]
+        public async Task<IActionResult> GetSoldAsync([FromRoute] Guid id)
+            => Ok(await service.GetSoldAsync(id));
+
+        [HttpGet("Sold/{carId}")]
+        public async Task<IActionResult> GetSoldAsync(
+            [FromRoute] Guid carId,
+            int quantity)
+                => Ok(await service.SoldAsync(carId, quantity));
+    }
+}
