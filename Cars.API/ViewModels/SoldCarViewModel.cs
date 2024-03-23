0c7fc2bd841a17077cb92ea2c5d36d54beecefc8
xomidov571 <nodirbekh56@gmail.com>
2024-03-23 12:40:38 +0500

    Successfully completed task #17

---
+++ Cars.API/ViewModels/SoldCarViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Cars.API.Entities;
+
+namespace Cars.API.ViewModels
+{
+    public class SoldCarViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid CarId { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public double TotalPrice { get; set; }
+        public DateTime SoldDate { get; set; }
+
+        public static explicit operator SoldCarViewModel(Sold sold)
+        {
+            return new SoldCarViewModel
+            {
+                Id = sold.Id,
+                CarId = sold.CarId,
+                Price = sold.Price,
+                Quantity = sold.Quantity,
+                TotalPrice = sold.TotalPrice,
+                SoldDate = sold.SoldDate
+            };
+        }
+    }
+};
